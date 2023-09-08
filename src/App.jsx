17811c4bf9072632e54a0eaa9d448df81c297ac4
Saif-09 <saifmd238@gmail.com>
2023-09-09 03:42:02 +0530

    Added comments according to funct

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import InputBox from "./components/InputBox";
 import useCurrencyConverter from "./hooks/useCurrency";
 
 function App() {
-  const [from, setFrom] = useState("usd");
-  const [to, setTo] = useState("inr");
-  const [amount, setAmount] = useState("");
-  const [convertedAmount, setConvertedAmount] = useState("");
-  const currencyData = useCurrencyConverter(from);
-  const currencyOptions = Object.keys(currencyData);
+  // Define states to manage input and conversion data
+  const [from, setFrom] = useState("usd"); // Selected "From" currency
+  const [to, setTo] = useState("inr"); // Selected "To" currency
+  const [amount, setAmount] = useState(""); // Amount to be converted
+  const [convertedAmount, setConvertedAmount] = useState(""); // Converted amount
+  const currencyData = useCurrencyConverter(from); // Fetch currency data
+  const currencyOptions = Object.keys(currencyData); // Extract currency options
 
+  // Function to convert the selected currency
   const convertCurrency = () => {
+    // Calculate and set the converted amount with 2 decimal places
     setConvertedAmount((amount * currencyData[to]).toFixed(2));
   };
 
+  // Function to swap "From" and "To" currencies
   const swap = () => {
+    // Swap the selected currencies
     setFrom(to);
     setTo(from);
+
+    // Swap the converted and input amounts (Note: This logic may need adjustments)
     setConvertedAmount(amount);
     setAmount(convertedAmount);
   };
@@ -26,15 +33,18 @@ function App() {
       <div className="bg-white shadow-md rounded-lg p-8 w-96">
         <h1 className="text-2xl font-semibold mb-4">Currency Converter</h1>
         <div className="mb-6">
+          {/* InputBox for "From" currency */}
           <InputBox
-            label="From"
-            onCurrencySelection={(currency) => setFrom(currency)}
-            selectedCurrency={from}
-            currencyOptions={currencyOptions}
-            amount={amount}
-            onAmountChange={(amount) => setAmount(amount)}
+            label="From" // Label for the input box (indicating "From" currency)
+            onCurrencySelection={(currency) => setFrom(currency)} // Function to update the "From" currency when the user selects a different currency
+            selectedCurrency={from} // Currently selected currency for the "From" input box (controlled by the 'from' state)
+            currencyOptions={currencyOptions} // Available currency options for the user to choose from
+            amount={amount} // Value of the input box, representing the amount to convert
+            onAmountChange={(amount) => setAmount(amount)} // Function to update the 'amount' state when the user changes the input value
           />
+
         </div>
+        {/* Swap button */}
         <button
           type="button"
           className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2 absolute left-1/2 -translate-x-1/2 -translate-y-1/2 mb-8"
@@ -43,15 +53,18 @@ function App() {
           Swap
         </button>
         <div className="mb-4">
+          {/* InputBox for "To" currency */}
           <InputBox
-            label="To"
-            amount={convertedAmount}
-            currencyOptions={currencyOptions}
-            selectedCurrency={to}
-            onCurrencySelection={(currency) => setTo(currency)}
-            amountDisable
+            label="To" // Label for the input box (indicating "To" currency)
+            amount={convertedAmount} // Value of the input box, representing the converted amount
+            currencyOptions={currencyOptions} // Available currency options for the user to choose from
+            selectedCurrency={to} // Currently selected currency for the "To" input box (controlled by the 'to' state)
+            onCurrencySelection={(currency) => setTo(currency)} // Function to update the "To" currency when the user selects a different currency
+            amountDisable // A boolean prop that indicates the input should be disabled (amount input in "To" box is disabled)
           />
+
         </div>
+        {/* Convert button */}
         <button
           type="button"
           className="w-full bg-blue-500 text-white px-4 py-2 rounded-md"
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,15 +1,15 @@
 import React, { useId } from 'react';
 
 const InputBox = ({
-    label,
-    onAmountChange,
-    amount,
-    currencyOptions = [],
-    selectedCurrency = "usd",
-    onCurrencySelection,
-    amountDisable = false,
+    label, // Label for the input field
+    onAmountChange, // Function to handle amount change
+    amount, // Amount input value
+    currencyOptions = [], // Available currency options
+    selectedCurrency = "usd", // Selected currency
+    onCurrencySelection, // Function to handle currency selection
+    amountDisable = false, // Whether the input should be disabled
 }) => {
-    const amountInputId = useId();
+    const amountInputId = useId(); // Generate a unique ID for the input field
 
     return (
         <div className="bg-white p-3 rounded-lg text-sm flex flex-col sm:flex-row items-center">
@@ -21,40 +21,41 @@ const InputBox = ({
                     {label}
                 </label>
                 <input
-                    value={amount}
-                    id={amountInputId}
-                    type="number"
-                    placeholder="Amount"
-                    className="outline-none bg-gray-100 py-1.5 w-full px-2 rounded-md"
+                    value={amount} // The value displayed in the input (controlled by the 'amount' prop)
+                    id={amountInputId} // An ID for the input element, often used for labeling and accessibility
+                    type="number" // Specifies the input type as 'number'
+                    placeholder="Amount" // Placeholder text displayed in the input when it's empty
+                    className="outline-none bg-gray-100 py-1.5 w-full px-2 rounded-md" // CSS classes defining the input's styling
                     onChange={(e) => {
-                        onAmountChange && onAmountChange(Number(e.target.value));
+                        onAmountChange && onAmountChange(Number(e.target.value)); // Function to handle changes in the input value and update 'amount' when the user types a new amount
                     }}
-                    disabled={amountDisable}
+                    disabled={amountDisable} // A boolean prop that indicates whether the input should be disabled (controlled by the 'amountDisable' prop)
                 />
+
             </div>
-            <div className="w-full sm:w-1/2 flex items-center justify-end flex-wrap  gap-1">
+            <div className="w-full sm:w-1/2 flex items-center justify-end flex-wrap gap-1">
                 <p className="text-gray-600 mb-2 w-full sm:w-auto">
                     Currency Type
                 </p>
                 <select
                     className="rounded-md px-1 py-1 bg-gray-100 cursor-pointer outline-none w-full sm:w-auto"
-                    value={selectedCurrency}
+                    value={selectedCurrency} // The currently selected currency (controlled by the 'selectedCurrency' prop)
                     onChange={(e) => {
-                        onCurrencySelection && onCurrencySelection(e.target.value);
+                        onCurrencySelection && onCurrencySelection(e.target.value); // Function to handle currency selection and update 'selectedCurrency' when the user changes the option
                     }}
-                    disabled={''}
+                    disabled={''} // A boolean prop (often used for disabling the select element, but currently an empty string is provided)
                 >
                     {currencyOptions.map((currency) => (
                         <option
                             key={currency}
                             value={currency}
                         >
-                            {currency.toUpperCase()}
+                            {currency.toUpperCase()} {/* Display the available currency options in uppercase */}
                         </option>
                     ))}
                 </select>
-            </div>
 
+            </div>
         </div>
     );
 }
